ea7980fc0e2f5413f36dfb297de9ab2bce1764c6
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-11-15 14:22:59 +0100

    mouseover mouseleave cursor

--- src/components/Mouse.js
+++ src/components/Mouse.js
@@ -8,11 +8,14 @@ const Mouse = () => {
 			cursor.style.top = e.y + 'px';
 			cursor.style.left = e.x + 'px';
 		});
+
 		document.querySelectorAll('.hover').forEach((link) => {
 			link.addEventListener('mouseover', () => {
 				cursor.classList.add('hovered');
 			});
+
 			link.addEventListener('mouseleave', () => {
+				cursor.style.transition = '.3s ease-out';
 				cursor.classList.remove('hovered');
 			});
 		});
