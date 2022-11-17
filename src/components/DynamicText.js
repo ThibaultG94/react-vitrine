2b4b5900251896ee8adab11cd366d182be4f4303
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-11-17 15:23:48 +0100

    dynamictext

--- src/components/DynamicText.js
+++ src/components/DynamicText.js
@@ -1,6 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const DynamicText = () => {
+	useEffect(() => {
+		const target = document.getElementById('text-target');
+		let array = ['simple', 'clear', 'smart', 'strong'];
+		let wordIndex = 0;
+		let letterIndex = 0;
+
+		const createLetter = async () => {
+			const letter = document.createElement('span');
+			target.appendChild(letter);
+
+			letter.classList.add('letter');
+			letter.style.opacity = '0';
+			letter.style.animation = 'anim 5s ease forwards';
+			letter.textContent = array[wordIndex][letterIndex];
+			setTimeout(() => {
+				letter.remove();
+			}, 2000);
+		};
+
+		const loop = async () => {
+			await setTimeout(() => {
+				if (wordIndex >= array.length) {
+					wordIndex = 0;
+					letterIndex = 0;
+					loop();
+				} else if (letterIndex < array[wordIndex].length) {
+					createLetter();
+					letterIndex++;
+					loop();
+				} else {
+					wordIndex++;
+					letterIndex = 0;
+					setTimeout(() => {
+						loop();
+					}, 2000);
+				}
+			}, 80);
+		};
+		loop();
+	}, []);
+
 	return (
 		<span className="dynamic-text">
 			<span>simply</span>
