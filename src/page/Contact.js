878142009449d378535d76d7516857aeeb74e9c6
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-11-21 16:57:19 +0100

    contact infos

--- src/page/Contact.js
+++ src/page/Contact.js
@@ -3,6 +3,7 @@ import Mouse from '../components/Mouse';
 import Navigation from '../components/Navigation';
 import Logo from '../components/Logo';
 import ContactForm from '../components/ContactForm';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const Contact = () => {
 	return (
@@ -12,6 +13,20 @@ const Contact = () => {
 				<Navigation />
 				<Logo />
 				<ContactForm />
+				<div className="contact-infos">
+					<div className="adress">
+						<div className="content">
+							<h4>adresse</h4>
+							<p>12 rue Laplace</p>
+							<p>64200 Biarritz</p>
+						</div>
+					</div>
+					<div className="phone">
+						<div className="content">
+							<h4>téléphone</h4>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
