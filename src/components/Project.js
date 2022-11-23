84f7b1a570ffb318ee05ff2b4ecfb091573123c9
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-11-23 13:56:49 +0100

    projects motion frame

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { projectsData } from '../data/projectsData';
+import { motion } from 'framer-motion';
 
 const Project = ({ projectNumber }) => {
 	const [currentProject, setCurrentProject] = useState(
@@ -15,8 +16,49 @@ const Project = ({ projectNumber }) => {
 		setSize('scale(' + (Math.random() + 0.7) + ')');
 	}, []);
 
+	const variants = {
+		initial: {
+			opacity: 0,
+			transition: { duration: 0.5 },
+			x: 200,
+		},
+		visible: {
+			opacity: 1,
+			x: 0,
+		},
+		exit: {
+			opacity: 0.4,
+			transition: { duration: 0.35 },
+			x: -800,
+		},
+	};
+
+	const transition = {
+		ease: [0.03, 0.07, 0.73, 0.9],
+		duration: 0.6,
+	};
+
+	const imgAnim = {
+		initial: {
+			opacity: 0,
+			x: Math.floor(Math.random() * 350 * (Math.random() > 0.4 ? 1 : -1)),
+			y: Math.floor(Math.random() * 120 * (Math.random() > 0.4 ? 1 : -1)),
+		},
+		visible: {
+			opacity: 1,
+			x: 0,
+			y: 0,
+		},
+	};
+
 	return (
-		<div className="project-main">
+		<motion.div
+			className="project-main"
+			initial="initial"
+			animate="visible"
+			exit="exit"
+			transition={transition}
+			variants={variants}>
 			<div className="project-content">
 				<h1>{currentProject.title}</h1>
 				<p>{currentProject.date}</p>
@@ -29,7 +71,12 @@ const Project = ({ projectNumber }) => {
 			<span
 				className="random-circle"
 				style={{ left, top, transform: size }}></span>
-			<div className="img-content">
+			<motion.div
+				className="img-content"
+				initial="initial"
+				animate="visible"
+				variants={imgAnim}
+				transition={{ duration: 1.2 }}>
 				<div className="img-container hover">
 					<span>
 						<h3>{currentProject.title}</h3>
@@ -50,8 +97,8 @@ const Project = ({ projectNumber }) => {
 						<span className="button">voir le site</span>
 					</a>
 				</div>
-			</div>
-		</div>
+			</motion.div>
+		</motion.div>
 	);
 };
 
